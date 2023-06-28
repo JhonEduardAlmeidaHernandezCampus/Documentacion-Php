029b8046b0881692893ef92234633ca947118a4b
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-06-28 18:46:25 -0500

    feat: :rocket: Definicion y estructura php
    Se inicia el repositorio dando la definicion y la estructura de php
    
    by: Jhon Hernandez

---
+++ doc.php
@@ -0,0 +1,26 @@
+<?php
+
+    print_r("1. DEFINICION DE PHP");
+
+    /*
+    PHP (acrónimo recursivo de PHP: Hypertext Preprocessor)
+    es un lenguaje de programación de código abierto utilizado 
+    principalmente para el desarrollo web. Es muy popular y
+    ampliamente utilizado, especialmente en la creación de 
+    aplicaciones web dinámicas.
+    */
+
+    print_r("ESTRUCTURA");
+
+    /*
+    La etiqueta <?php ?> se utiliza en PHP para delimitar 
+    el inicio y el final de un bloque de código PHP 
+    */
+
+    /*ejemplo*/
+    /*
+    <?php
+        echo "Hello World!";
+    ?>
+    */
+?>
