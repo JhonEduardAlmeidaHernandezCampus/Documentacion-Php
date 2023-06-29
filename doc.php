1ea07f11c4f5de1de1ac997f272867cf930c6a33
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-06-28 20:37:19 -0500

    docs: :memo: Arreglos en Php
    Estos son algunos tipos de arreglos en php
    
    by: Jhon Hernandez

--- doc.php
+++ doc.php
@@ -1,44 +1,60 @@
 <?php
 
-    print_r("OPERADORES");
-
-
-                    /* Operadores aritméticos */
-
-    /*
-    Suma (+): Se utiliza para sumar dos valores.
-    */
-
-    $resultado = 10 + 5; // $resultado será igual a 15
-
-
+    print_r("ARREGLOS");
 
 
     /*
-    Resta (-): Se utiliza para restar un valor de otro.
+    Arreglos indexados:
+        Los arreglos indexados son aquellos en los que los elementos se acceden mediante un índice numérico.
+        Los índices comienzan desde cero y se incrementan en una unidad.
     */
 
-    $resultado = 10 - 5; // $resultado será igual a 5
-    
-    
-
-
-    /*
-    Multiplicación (*): Se utiliza para multiplicar dos valores.
-    */
-
-    $resultado = 10 * 5; // $resultado será igual a 50
+    $arreglo = array("Manzana", "Banana", "Naranja");
+    echo $arreglo[0]; // Resultado: "Manzana"
 
 
     
 
     /*
-    División (/): Se utiliza para dividir un valor por otro.
+    Arreglos asociativos:
+        Los arreglos asociativos son aquellos en los que los elementos se acceden mediante claves (nombres o etiquetas) en lugar de índices numéricos.
+        Cada elemento del arreglo tiene una clave única asociada.
     */
 
-    $resultado = 10 / 5; // $resultado será igual a 2
+    $arreglo = array("nombre" => "Juan", "edad" => 30, "ciudad" => "México");
+    echo $arreglo["nombre"]; // Resultado: "Juan"
+    
+    
+
+
+    /*
+    Arreglos multidimensionales:
+        Los arreglos multidimensionales son aquellos que contienen otros arreglos como elementos.
+        Permiten crear estructuras de datos más complejas, como matrices o tablas.
+    */
+
+    $arreglo = array(
+        array("Manzana", "Banana"),
+        array("Naranja", "Limón"),
+        array("Fresa", "Cereza")
+    );
+    echo $arreglo[1][0]; // Resultado: "Naranja"
+    
 
     
+
+    /*
+    Arreglos de tipo mixto:
+        Los arreglos de tipo mixto son aquellos en los que se combinan elementos indexados y asociativos.
+        Pueden contener tanto valores numéricos como elementos con claves asociativas.
+    */
+
+    $arreglo = array("Manzana", "nombre" => "Juan", 3 => "Naranja");
+    echo $arreglo[0]; // Resultado: "Manzana"
+    echo $arreglo["nombre"]; // Resultado: "Juan"
+    echo $arreglo[3]; // Resultado: "Naranja"
+
+
 
 
     /*
@@ -50,168 +66,79 @@
     
 
 
-                    /* Operadores comparación */
+                    /* Estructuras de control */
+
 
     /*
-    Asignación (=): Se utiliza para asignar un valor a una variable.
+    Estructura de control if-else:
+        Permite ejecutar un bloque de código si se cumple una condición, y otro bloque de código si no se cumple.
     */
 
-    $x = 10; // La variable $x tendrá el valor 10
-
-
-      
-
-    /*
-    Asignación de suma (+=): Se utiliza para sumar un valor a una variable y asignar el resultado a la misma variable.
-    */
-
-    $x = 5;
-    $x += 3; // Equivale a $x = $x + 3; La variable $x tendrá el valor 8
+    $edad = 25;
+    if ($edad >= 18) {
+        echo "Eres mayor de edad.";
+    } else {
+        echo "Eres menor de edad.";
+    }
 
 
 
 
     /*
-    Asignación de resta (-=): Se utiliza para restar un valor a una variable y asignar el resultado a la misma variable.
+    Estructura de control switch:
+        Permite evaluar múltiples casos y ejecutar un bloque de código según el caso que se cumpla.
     */
 
-    $x = 10;
-    $x -= 4; // Equivale a $x = $x - 4; La variable $x tendrá el valor 6    
+    $dia = "Lunes";
+    switch ($dia) {
+        case "Lunes":
+            echo "Hoy es lunes.";
+            break;
+        case "Martes":
+            echo "Hoy es martes.";
+            break;
+        default:
+            echo "Hoy no es ni lunes ni martes.";
+    }
 
 
 
 
     /*
-    Asignación de multiplicación (*=): Se utiliza para multiplicar una variable por un valor y asignar el resultado a la misma variable.
+    Estructura de control for:
+        Permite ejecutar un bloque de código un número específico de veces, controlado por un contador.
     */
 
-    $x = 3;
-    $x *= 2; // Equivale a $x = $x * 2; La variable $x tendrá el valor 6
-
-
-
-
-    /*
-    Asignación de división (/=): Se utiliza para dividir una variable por un valor y asignar el resultado a la misma variable.
-    */
-
-    $x = 10;
-    $x /= 5; // Equivale a $x = $x / 5; La variable $x tendrá el valor 2
-
-
-
-
-    /*
-    Asignación de división (/=): Se utiliza para dividir una variable por un valor y asignar el resultado a la misma variable.
-    */
-
-    $x = 10;
-    $x %= 3; // Equivale a $x = $x % 3; La variable $x tendrá el valor 1 (resto de la división de 10 entre 3)
-
-    
-
-
-                        /* Operadores comparación */
-
-
-    /*
-    Igual (==): Se utiliza para comprobar si dos valores son iguales.
-    */
-
-    $x = 5;
-    $y = 5;
-    $resultado = ($x == $y); // $resultado será igual a true
-
-
-
-
-    /*
-    Desigualdad (!=): Compara si dos valores no son iguales.
-    */
-
-    $x = 5;
-    $y = 10;
-    $resultado = ($x != $y); // $resultado será igual a true
-
-    
-
-
-    /*
-    Mayor que (>): Compara si el primer valor es mayor que el segundo.
-    */
-
-    $x = 10;
-    $y = 5;
-    $resultado = ($x > $y); // $resultado será igual a true
-
-
-
-
-    /*
-    Menor que (<): Compara si el primer valor es menor que el segundo.
-    */
-
-    $x = 10;
-    $y = 5;
-    $resultado = ($x < $y); // $resultado será igual a false
+    for ($i = 1; $i <= 5; $i++) {
+        echo "El valor de i es: " . $i . "<br>";
+    }
     
 
 
 
     /*
-    Mayor o igual que (>=): Compara si el primer valor es mayor o igual que el segundo.
+    Estructura de control while:
+        Permite ejecutar un bloque de código mientras se cumpla una condición.
     */
 
-    $x = 10;
-    $y = 10;
-    $resultado = ($x >= $y); // $resultado será igual a true
+    $contador = 1;
+    while ($contador <= 5) {
+        echo "El valor del contador es: " . $contador . "<br>";
+        $contador++;
+    }
     
 
 
 
     /*
-    Menor o igual que (<=): Compara si el primer valor es menor o igual que el segundo.
+    Estructura de control do-while:
+        Permite ejecutar un bloque de código al menos una vez y luego repetirlo mientras se cumpla una condición.
     */
 
-    $x = 10;
-    $y = 5;
-    $resultado = ($x <= $y); // $resultado será igual a false
-    
-
-
-
-
-
-                            /* Operadores comparación */
-
-
-    /*
-    Y lógico (&& o and): Verifica si se cumplen dos condiciones.
-    */
-
-    $x = 5;
-    $y = 10;
-    $resultado = ($x > 0) && ($y < 20); // $resultado será igual a true, ya que ambas condiciones se cumplen
-    
-
-
-
-    /*
-    O lógico (|| o or): Verifica si se cumple al menos una de dos condiciones.
-    */
-
-    $x = 5;
-    $y = 10;
-    $resultado = ($x > 0) || ($y < 5); // $resultado será igual a true, ya que al menos una de las condiciones se cumple (la primera condición es verdadera)
-
-
-
-
-    /*
-    Negación lógica (! o not): Invierte el valor de una condición.
-    */
-    
-    $x = 5;
-    $resultado = !($x > 0); // $resultado será igual a false, ya que la condición original es verdadera, pero se invierte con el operador de negación
+    $contador = 1;
+    do {
+        echo "El valor del contador es: " . $contador . "<br>";
+        $contador++;
+    } while ($contador <= 5);
 
 ?>
