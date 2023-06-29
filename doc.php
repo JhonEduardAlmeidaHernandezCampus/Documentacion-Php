563c10fc3b16db05bacd683f8a54465941a6f5a1
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-06-28 19:40:18 -0500

    docs: :memo: Constantes Php
    Algunos ejemplos de constante en php
    
    by: Jhon Hernandez

--- doc.php
+++ doc.php
@@ -1,197 +1,109 @@
 <?php
 
-    print_r("VARIABLES");
+    print_r("CONSTANTES");
 
     /*
-    Puedes utilizar variables para almacenar y manipular diferentes tipos de datos
-    como cadenas de texto, números, valores booleanos y más
-    Además, las variables en PHP pueden cambiar de tipo durante la ejecución del programa 
-    ya que PHP es un lenguaje de tipado dinámico.
+    es un identificador (nombre) asociado a un valor que no puede cambiar durante la ejecución del script.
     */
 
    
 
 
     /*
-    Enteros (int): Representan números enteros sin decimales, positivos o negativos.
+    Constantes numéricas:
     */
 
-    $edad = 25;
-    $cantidad = -10;
+    define("PI", 3.1416);
+    define("GRAVEDAD", 9.8);
+
+
+
+
+    /*
+    Constantes de cadena de texto:
+    */
+
+    $define("SALUDO", "Hola, bienvenido");
+    define("NOMBRE_EMPRESA", "Mi Empresa");    
 
     
 
 
     /*
-    Flotantes (float): Representan números con decimales.
+    Constantes booleanas:
     */
 
-    $precio = 12.99;
-    $pi = 3.1416;
+    define("VERDADERO", true);
+    define("FALSO", false);
+
+
+    
+
+    /*
+    Constantes de arreglo:
+    */
+
+    define("COLORES", ['rojo', 'verde', 'azul']);
+    define("MESES", array('enero', 'febrero', 'marzo'));
 
     
 
 
     /*
-    Cadenas (string): Representan secuencias de caracteres entre comillas simples o dobles.
+    Constantes predefinidas:
     */
 
-    $nombre = "Juan";
-    $mensaje = '¡Hola, bienvenido!';
+    echo PHP_VERSION; // Muestra la versión de PHP instalada
+    echo PHP_OS; // Muestra el sistema operativo en el que se está ejecutando PHP
 
     
 
 
     /*
-    Booleanos (bool): Representan un valor verdadero (true) o falso (false).
+    Constantes de fecha y tiempo:
     */
 
-    $activo = true;
-    $validado = false;
+    define("FECHA_ACTUAL", date("Y-m-d"));
+    define("HORA_ACTUAL", date("H:i:s"));
 
-    
-
-
-    /*
-    Arrays: Almacenan múltiples valores en una sola variable.
-    */
-
-    $numeros = array(1, 2, 3, 4, 5);
-    $frutas = ['manzana', 'naranja', 'pera'];
-
-    
-
-
-    /*
-    Objetos (object): Representan una instancia de una clase y contienen propiedades y métodos.
-    */
-
-    class Persona {
-        public $nombre;
-        public $edad;
-    }
-      
-      $persona = new Persona();
-      $persona->nombre = "Juan";
-      $persona->edad = 25;
 
       
 
-
     /*
-    NULL: Representa la ausencia de un valor.
+    Constantes de rutas de archivos y directorios:
     */
 
-    $variable = null;
-
-
-
-    /*
-    $_GET: Contiene los parámetros de la URL enviados mediante el método GET.
-    */
-
-    // URL: ejemplo.php?nombre=Juan&edad=25
-    echo $_GET['nombre']; // Salida: Juan
-    echo $_GET['edad']; // Salida: 25
+    define("RUTA_ARCHIVOS", "/var/www/archivos/");
+    define("RUTA_IMAGENES", RUTA_ARCHIVOS . "imagenes/");
 
 
 
 
     /*
-    $_POST: Contiene los datos enviados a través de un formulario HTML mediante el método POST.
+    Constantes de expresiones regulares:
     */
 
-    // Formulario HTML: <form action="procesar.php" method="POST">...</form>
-    $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
+    define("PATRON_EMAIL", "/^[\w-]+(\.[\w-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/i");
 
 
 
 
     /*
-    $_SESSION: Almacena y mantiene variables de sesión a lo largo de diferentes páginas.
+    Constantes de configuración:
     */
 
-    // Iniciar sesión
-    session_start();
-
-    // Almacenar datos en la sesión
-    $_SESSION['usuario'] = 'Juan';
-    $_SESSION['rol'] = 'admin';
-
-    // Acceder a los datos de la sesión
-    echo $_SESSION['usuario']; // Salida: Juan
-    echo $_SESSION['rol']; // Salida: admin
+    define("TAMANO_MAXIMO", 1024); // Tamaño máximo permitido en kilobytes
+    define("NUMERO_INTENTOS", 3); // Número máximo de intentos permitidos
 
 
 
 
     /*
-    $_COOKIE: Contiene los valores de las cookies enviadas al servidor.
+    Constantes de valores especiales:
     */
 
-    // Obtener el valor de una cookie
-    $idioma = $_COOKIE['idioma'];
-
-    // Configurar una cookie
-    setcookie('idioma', 'es', time() + 3600); // Caduca en 1 hora
-
-
-
-
-    /*
-    $_SERVER: Contiene información sobre el entorno del servidor y la solicitud HTTP actual.
-    */
-
-    echo $_SERVER['SERVER_NAME']; // Salida: localhost
-    echo $_SERVER['REQUEST_METHOD']; // Salida: GET o POST
-    
-
-
-
-
-    /*
-    $_FILES: Contiene información sobre los archivos subidos al servidor a través de un formulario HTML.
-    */
-
-    // Formulario HTML: <form action="procesar.php" method="POST" enctype="multipart/form-data">...</form>
-    $archivo = $_FILES['archivo']['name'];
-    $tipo = $_FILES['archivo']['type'];
-    $tamaño = $_FILES['archivo']['size'];
-
-
-
-
-    /*
-    $_ENV: Contiene variables de entorno del sistema.
-    */
-
-    $directorioTemp = $_ENV['TMP'];
-    $usuario = $_ENV['USER'];
-
-
-
-
-    /*
-    $_REQUEST: Contiene los valores de las variables enviadas por GET, POST y COOKIE.
-    */
-
-    $nombre = $_REQUEST['nombre'];
-    $edad = $_REQUEST['edad'];
-    
-
-
-
-    /*
-    $GLOBALS: Contiene todas las variables globales definidas en el ámbito global del script.
-    */
-
-    $globalVariable = 'Hola, soy una variable global';
-
-    function imprimirGlobal() {
-      echo $GLOBALS['globalVariable'];
-    }
-    
-    imprimirGlobal(); // Salida: Hola, soy una variable global
+    define("NULO", null);
+    define("INFINITO_POSITIVO", INF);
+    define("INFINITO_NEGATIVO", -INF);
     
 ?>
